916ef20032cee14331743835518b3590f41c5dbd
aiavenak <aiavenak@indiana.edu>
2018-07-02 11:59:58 -0400

    Add files via upload

---
+++ dateParser.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import socket   
+
+now = datetime.now()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+
+
