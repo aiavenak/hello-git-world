8bc1e1f94f89c6158ef03de788411ee87409dc7e
aiavenak <aiavenak@indiana.edu>
2018-07-02 12:52:10 -0400

    edit 2
    Uploaded updated file from avenak

--- dateParser.py
+++ dateParser.py
@@ -1,16 +1,7 @@
-from datetime import datetime
 import socket   
 
-print "This is a simple example"
-now = datetime.now()
-mm = str(now.month)
-dd = str(now.day)
-yyyy = str(now.year)
-hour = str(now.hour)
-mi = str(now.minute)
-ss = str(now.second)
-print "the date is: "
-print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+import numpy as np
+import matplotlib.pyplot as plt
 
 print "-" * 25
 
@@ -18,5 +9,14 @@ hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)   
 print("Your Computer Name is:" + hostname)   
 print("Your Computer IP Address is:" + IPAddr)   
+
+print "-" * 25
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
 
 
