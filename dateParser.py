37d801b905881230309bde42e2940bee3fbffa39
aiavenak <aiavenak@indiana.edu>
2018-07-02 12:13:53 -0400

    update
    Added a title and a print line

--- dateParser.py
+++ dateParser.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import socket   
 
+print "This is a simple example"
 now = datetime.now()
 mm = str(now.month)
 dd = str(now.day)
@@ -8,6 +9,7 @@ yyyy = str(now.year)
 hour = str(now.hour)
 mi = str(now.minute)
 ss = str(now.second)
+print "the date is: "
 print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
 
 print "-" * 25
