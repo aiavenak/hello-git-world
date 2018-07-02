9186a80d574d73ebc7082d576257c5fe2a09878e
aiavenak <aiavenak@indiana.edu>
2018-07-02 12:56:57 -0400

    added titles to sections
    Titles describe what each code section is doing

--- dateParser.py
+++ dateParser.py
@@ -3,14 +3,18 @@ import socket
 import numpy as np
 import matplotlib.pyplot as plt
 
-print "-" * 25
+print "-" * 15
+print "computer name and IP address Example"
+print "-" * 15
 
 hostname = socket.gethostname()   
 IPAddr = socket.gethostbyname(hostname)   
 print("Your Computer Name is:" + hostname)   
 print("Your Computer IP Address is:" + IPAddr)   
 
-print "-" * 25
+print "-" * 15
+print "How to plot simple functions"
+print "-" * 15
 
 # evenly sampled time at 200ms intervals
 t = np.arange(0., 5., 0.2)
